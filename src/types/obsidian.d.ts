2a3d343c5ad335b08214ea9c5d17818007cc9fca
Ivan Bravo <ivanbravoprofessional@gmail.com>
2026-09-23 21:23:46 -0700

    perf: overhaul Canvas webview lifecycle and thumbnail generation

--- src/types/obsidian.d.ts
+++ src/types/obsidian.d.ts
@@ -1,21 +1,35 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import 'obsidian'
 
 declare module 'obsidian' {
+  interface CanvasThumbnailImage {
+    getSize(): { width: number; height: number }
+    isEmpty(): boolean
+    resize(options: { width: number; height: number; quality: 'good' }): CanvasThumbnailImage
+    toJPEG(quality: number): ArrayBuffer
+  }
+
   interface WorkspaceLeaf {
     rebuildView(): void
   }
 
   interface Workspace {
     trigger(name: string): void
-
     on(name: string, cb: () => unknown): EventRef
   }
 
+  interface CanvasViewportBBox {
+    minX: number
+    minY: number
+    maxX: number
+    maxY: number
+  }
+
   interface Canvas {
+    wrapperEl: HTMLElement
     initialize(...args: unknown[]): unknown
     recreateFrame(...args: unknown[]): unknown
     createLinkNode(...args: unknown[]): LinkNode
+    getViewportBBox?(): CanvasViewportBBox
   }
 
   interface CanvasLeaf extends WorkspaceLeaf {
@@ -26,13 +40,42 @@ declare module 'obsidian' {
     canvas: Canvas
   }
 
+  interface CanvasWebviewElement extends HTMLElement {
+    capturePage(): Promise<CanvasThumbnailImage>
+    executeJavaScript(code: string): Promise<unknown>
+    getTitle(): string
+    insertCSS(css: string): Promise<string>
+    setAudioMuted?(muted: boolean): void
+    stop?(): void
+  }
+
   interface LinkNode {
+    id: string
     url: string
-    constructor: LinkNodeConstructor
+    nodeEl: HTMLElement
+    contentEl: HTMLElement & { doc: Document }
+    frameEl: CanvasWebviewElement | null
+    canvas?: Canvas
+    x?: number
+    y?: number
+    width?: number
+    height?: number
+    isContentMounted?: boolean
+    _initializing?: boolean
+    _previewImageEl?: HTMLImageElement | null
+    initialize(...args: unknown[]): unknown
+    mountContent(...args: unknown[]): unknown
+    updateBreakpoint(...args: unknown[]): unknown
+    setData(...args: unknown[]): unknown
+    recreateFrame(...args: unknown[]): unknown
+    _getThumbnailPath(): string
+    _getMetadataPath(): string
+    _saveThumbnail(): Promise<boolean>
+    updateNodeLabel(title: string): void
   }
 
   interface LinkNodeConstructor {
-    prototype: (...args: unknown[]) => unknown
+    prototype: LinkNode
   }
 
   interface Vault {
