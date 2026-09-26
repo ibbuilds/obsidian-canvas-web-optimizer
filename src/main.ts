35edf20d234956b711190070f058c3d98871b987
Ivan Bravo <ivanbravoprofessional@gmail.com>
2026-09-25 18:23:42 -0700

    style: format selective settle diagnostics

--- src/main.ts
+++ src/main.ts
@@ -2513,8 +2513,7 @@ export default class CanvasWebOptimizerPlugin extends Plugin {
         averagePaintReadyMs: this.localBrowserRenderer?.averagePaintReadyMs ?? 0,
         averageVisualSettleMs: this.localBrowserRenderer?.averageVisualSettleMs ?? 0,
         visualSettleMaxOuts: this.localBrowserRenderer?.visualSettleMaxOutCount ?? 0,
-        visualSettleComplexPages:
-          this.localBrowserRenderer?.visualSettleComplexPageCount ?? 0,
+        visualSettleComplexPages: this.localBrowserRenderer?.visualSettleComplexPageCount ?? 0,
         loaderBypasses: this.localBrowserRenderer?.loaderBypassCount ?? 0,
         cookieCleanupActions: this.localBrowserRenderer?.cookieCleanupActionCount ?? 0,
         averageScreenshotMs: this.localBrowserRenderer?.averageScreenshotMs ?? 0,
